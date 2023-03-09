c613c1954dfd8cfc54e8da5e336cc6b02192c337
yxuanzhang <yxuanzhang@tencent.com>
2023-03-09 11:32:25 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,33 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import App from './App.vue'
+import router from './router'
+import { usePermissStore } from './store/permiss'
+import 'element-plus/dist/index.css'
+import './assets/css/icon.css'
 
-import App from "./App.vue";
-import router from "./router";
+const app = createApp(App)
 
-import "./assets/main.css";
+app.use(createPinia())
+app.use(router)
+app.use(ElementPlus, {
+    locale: zhCn,
+})
+// 注册elementplus图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+// 自定义权限指令
+const permiss = usePermissStore()
+app.directive('permiss', {
+    mounted(el, binding) {
+        if (!permiss.key.includes(String(binding.value))) {
+            el['hidden'] = true;
+        }
+    }
+})
 
-const app = createApp(App);
-
-app.use(createPinia());
-app.use(router);
-
-app.mount("#app");
+app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
-import { fileURLToPath, URL } from "node:url";
-
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
-
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import VueSetupExtend from 'vite-plugin-vue-setup-extend';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
+	base: './',
+	plugins: [
+		vue(),
+		VueSetupExtend(),
+		AutoImport({
+			resolvers: [ElementPlusResolver()]
+		}),
+		Components({
+			resolvers: [ElementPlusResolver()]
+		})
+	],
+	optimizeDeps: {
+		include: ['schart.js']
+	}
 });
